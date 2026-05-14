5bcea339d331a6ea4d68d090e3a40a5ce80f10a0
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-05-14 23:01:52 +0530

    update

--- car-rent/app.js
+++ car-rent/app.js
@@ -1,6 +1,13 @@
 const menuIcon = document.querySelector(".header-nav .ham-burger");
 const navLinks = document.querySelector(".header-nav nav ul");
+const logInBtn = document.querySelector(".nav-action .login-btn");
 
 menuIcon.addEventListener("click", () =>{
     navLinks.classList.toggle("active");
 })
+
+function fun(){
+    alert("website is in process !!!!")
+}
+
+logInBtn.addEventListener("click",fun);
